11d4b0d64ee1f9f7708e25f1975c51b505e738cf
JysonKhan <91392522+JysonKhan@users.noreply.github.com>
2021-09-26 01:40:39 -0400

    Update RMHP.js

--- scripts/RMHP.js
+++ scripts/RMHP.js
@@ -1,13 +1,16 @@
 Hooks.on("createToken", (TDoc, t1, t2) => {
-  if (TDoc.actor.type === "NPC") {
-    let HD = TDoc.actor.data.data.attributes.hitDice.value;
-    let HP = new Roll(HD, (data = {}), (options = {}));
-    HP.evaluate();
-    if (HP.total < 1) {
-      HP.total = 1;
-    }
-    TDoc.actor.data.data.attributes.hp.value = HP.total;
-    TDoc.actor.data.data.attributes.hp.min = HP.total;
-    TDoc.actor.data.data.attributes.hp.max = HP.total;
-  }
+	
+
+	if (TDoc.actor.type === "NPC") {
+		let HD = TDoc.actor.data.data.attributes.hitDice.value;
+		let HP = new Roll(HD, data = {}, options = {});
+		HP.evaluate();
+		let tHP = HP.total;
+		console.log (tHP);
+		if (tHP < 1) {tHP = 1;}
+		console.log (tHP);
+		TDoc.actor.data.data.attributes.hp.value = tHP;
+		TDoc.actor.data.data.attributes.hp.min = tHP;
+		TDoc.actor.data.data.attributes.hp.max = tHP;
+	}
 });
