0189b723187bb1468452585bbb06c68b95d4a518
Glitch (randommohp) <none>
2021-09-26 03:56:41 +0000

    🍁🌶 Updated with Glitch

--- scripts/RMHP.js
+++ scripts/RMHP.js
@@ -1,16 +1,13 @@
 Hooks.on("createToken", (TDoc, t1, t2) => {
-	
-
-console.log (TDoc.actor.type); 
-
-
-	if (TDoc.actor.type === "NPC") {
-		let HD = TDoc.actor.data.data.attributes.hitDice.value;
-		let HP = new Roll(HD, data = {}, options = {});
-		HP.evaluate();
-		if (HP.total <= 0) {HP.total = 1;}
-		TDoc.actor.data.data.attributes.hp.value = HP.total;
-		TDoc.actor.data.data.attributes.hp.min = HP.total;
-		TDoc.actor.data.data.attributes.hp.max = HP.total;
-	}
+  if (TDoc.actor.type === "NPC") {
+    let HD = TDoc.actor.data.data.attributes.hitDice.value;
+    let HP = new Roll(HD, (data = {}), (options = {}));
+    HP.evaluate();
+    if (HP.total < 1) {
+      HP.total = 1;
+    }
+    TDoc.actor.data.data.attributes.hp.value = HP.total;
+    TDoc.actor.data.data.attributes.hp.min = HP.total;
+    TDoc.actor.data.data.attributes.hp.max = HP.total;
+  }
 });
