c693116202b9b78730b13f6c208dec033d055ca8
JysonKhan <91392522+JysonKhan@users.noreply.github.com>
2021-09-26 01:47:10 -0400

    Update RMHP.js
    Fix for 0 HP NPC's spawning

--- scripts/RMHP.js
+++ scripts/RMHP.js
@@ -1,14 +1,17 @@
+// Looks at every instance a token is placed on the canvas 
 Hooks.on("createToken", (TDoc, t1, t2) => {
 	
-
+        // Checks if its a NPC type of actor
 	if (TDoc.actor.type === "NPC") {
+		// getting the NPC Hit Dice
 		let HD = TDoc.actor.data.data.attributes.hitDice.value;
+		// rolling the NPC's hit points
 		let HP = new Roll(HD, data = {}, options = {});
 		HP.evaluate();
+		// doing a little error checking to prevent any 0 HP mobs from spawning
 		let tHP = HP.total;
-		console.log (tHP);
 		if (tHP < 1) {tHP = 1;}
-		console.log (tHP);
+		// assigning the random HP amount to the proper fields
 		TDoc.actor.data.data.attributes.hp.value = tHP;
 		TDoc.actor.data.data.attributes.hp.min = tHP;
 		TDoc.actor.data.data.attributes.hp.max = tHP;
