4e84eb7201bf53b1eb5a7fc7db79a88964c4eedb
JysonKhan <91392522+JysonKhan@users.noreply.github.com>
2021-09-25 20:15:50 -0400

    Update RMHP.js

--- scripts/RMHP.js
+++ scripts/RMHP.js
@@ -1,16 +1,15 @@
-var HD; 
-var HP;
+Hooks.on("createToken", (TDoc, t1, t2) => {
+	
+
+console.log (TDoc.actor.type); 
 
 
-Hooks.call("dropCanvasData", this, data);
-
-console.log this.entity.data.type;
-
-if (this.entity.data.type === "NPC")
-{
-	let HD = this.attributes.hitdice.value;
-	let HP = new roll(HD).roll().total;
-	let this.attributes.hp.value = HP;
-	let this.attributes.hp.min = HP;
-	let this.attributes.hp.max = HP;
-}
+	if (TDoc.actor.type === "NPC") {
+		let HD = TDoc.actor.data.data.attributes.hitDice.value;
+		let HP = new Roll(HD, data = {}, options = {});
+		HP.evaluate();
+		TDoc.actor.data.data.attributes.hp.value = HP.total;
+		TDoc.actor.data.data.attributes.hp.min = HP.total;
+		TDoc.actor.data.data.attributes.hp.max = HP.total;
+	}
+});
