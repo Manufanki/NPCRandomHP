e176c9e66fdbc6c87094cce77ee0861d9595fdb6
Glitch (randommohp) <none>
2021-09-26 05:59:33 +0000

    👨‍❤️‍💋‍👨🛀 Updated with Glitch

--- scripts/RMHP.js
+++ scripts/RMHP.js
@@ -1,13 +1,19 @@
+// Looks at every instance a token is placed on the canvas 
 Hooks.on("createToken", (TDoc, t1, t2) => {
-  if (TDoc.actor.type === "NPC") {
-    let HD = TDoc.actor.data.data.attributes.hitDice.value;
-    let HP = new Roll(HD, (data = {}), (options = {}));
-    HP.evaluate();
-    if (HP.total < 1) {
-      HP.total = 1;
-    }
-    TDoc.actor.data.data.attributes.hp.value = HP.total;
-    TDoc.actor.data.data.attributes.hp.min = HP.total;
-    TDoc.actor.data.data.attributes.hp.max = HP.total;
-  }
+	
+        // Checks if its a NPC type of actor
+	if (TDoc.actor.type === "NPC") {
+		// getting the NPC Hit Dice
+		let HD = TDoc.actor.data.data.attributes.hitDice.value;
+		// rolling the NPC's hit points
+		let HP = new Roll(HD, data = {}, options = {});
+		HP.evaluate();
+		// doing a little error checking to prevent any 0 HP mobs from spawning
+		let tHP = HP.total;
+		if (tHP < 1) {tHP = 1;}
+		// assigning the random HP amount to the proper fields
+		TDoc.actor.data.data.attributes.hp.value = tHP;
+		TDoc.actor.data.data.attributes.hp.min = tHP;
+		TDoc.actor.data.data.attributes.hp.max = tHP;
+	}
 });
