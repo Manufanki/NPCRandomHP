8f052a8e89f51ad34635168f5314dcd516f16e44
Manufanki <105508263+Manufanki@users.noreply.github.com>
2023-11-24 11:53:56 +0100

    Update RMHP.js

--- scripts/RMHP.js
+++ scripts/RMHP.js
@@ -1,19 +1,28 @@
-// Looks at every instance a token is placed on the canvas 
-Hooks.on("createToken", (TDoc, t1, t2) => {
-	
-        // Checks if its a NPC type of actor
-	if (TDoc.actor.type === "NPC") {
-		// getting the NPC Hit Dice
-		let HD = TDoc.actor.data.data.attributes.hitDice.value;
-		// rolling the NPC's hit points
-		let HP = new Roll(HD, data = {}, options = {});
-		HP.evaluate();
-		// doing a little error checking to prevent any 0 HP mobs from spawning
-		let tHP = HP.total;
-		if (tHP < 1) {tHP = 1;}
-		// assigning the random HP amount to the proper fields
-		TDoc.actor.data.data.attributes.hp.value = tHP;
-		TDoc.actor.data.data.attributes.hp.max = tHP;
-		TDoc.update();
+Hooks.on("createToken", (token, tokenData, options, userId) => {
+	rollHP(token);
+  });
+  
+async function rollHP(token) {
+	if (token.actor && token.actor.data.type === "npc") {
+	  console.log(`TEST: New token is linked to an NPC`);
+  
+	  // Getting the NPC Hit Dice formula
+	  let formula = token.actor.data.data.attributes.hp.formula;
+  
+	  // Rolling the NPC's hit points
+	  let HP = new Roll(formula);
+	  await HP.evaluate();
+  
+	  // Doing error checking to prevent any 0 HP mobs from spawning
+	  let tHP = HP.total;
+	  if (tHP < 1) {
+		tHP = 1;
+	  }
+  
+	  // Assigning the random HP amount to the proper fields
+	  token.actor.data.data.attributes.hp.value = tHP;
+	  token.actor.data.data.attributes.hp.max = tHP;
+  
+	  console.log(`TEST: Set HP for ${token.actor.name} to ${tHP}`);
 	}
-});
+  }
